50c2572df72a13788dcf851db39b9c7dbb886ac6
Benjamin Webster <2102183@utrivieramaya.edu.mx>
2023-07-04 18:28:30 -0500

    login_regis_colore

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page{
+
+  selectedTab: string = 'tab1';
 
   email: string= "";
   password: string = "";
