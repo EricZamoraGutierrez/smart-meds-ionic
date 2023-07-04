2ef27561043ff5d1412e913890a2ea8688640f36
OscarC18 <2002024@utrivieramaya.edu.mx>
2023-07-04 17:19:56 -0600

    Profilesi

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  imageSource: string | ArrayBuffer | null = null;
+
+  displayImage(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          this.imageSource = reader.result;
+        }
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
 
   constructor() {}
 
