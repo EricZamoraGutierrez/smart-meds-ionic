d54db492eed7f924d474414af212f5e958b09850
OscarC18 <2002024@utrivieramaya.edu.mx>
2023-07-04 18:10:53 -0600

    ValidationOscar

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
@@ -13,7 +14,8 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab3PageRoutingModule
+    Tab3PageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [Tab3Page]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
@@ -6,8 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  myForm: FormGroup;
   imageSource: string | ArrayBuffer | null = null;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern(/^\S+@\S+$/)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z]).{8,}$/)]],
+      phone: ['', Validators.required]
+    });
+  }
+//es para poder agregar la imagen
   displayImage(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files[0]) {
@@ -20,7 +31,15 @@ export class Tab3Page {
       reader.readAsDataURL(input.files[0]);
     }
   }
+  submitForm(): void {
+    if (this.myForm.valid) {
+      console.log('Formulario valido xd');
+    } else {
+      console.log('Formulario teleton');
+    }
+  }
 
-  constructor() {}
-
+  isFormValid(): boolean {
+    return this.myForm.valid;
+  }
 }
