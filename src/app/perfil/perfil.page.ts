ff98183f25aad1f9bdaf2ecd3ed69bf48ad7209f
Benjamin Webster <2102183@utrivieramaya.edu.mx>
2023-07-10 16:17:26 -0500

    coambion

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router'; // Importar el Router para poder navegar a otra página
 
 @Component({
   selector: 'app-perfil',
@@ -11,13 +12,17 @@ export class PerfilPage {
   imageSource: string | ArrayBuffer | null = null;
   formData: any[] = []; // Arreglo para almacenar los datos del formulario
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,private router: Router) {
     this.myForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.pattern(/^\S+@\S+$/)]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.[a-z])(?=.[A-Z]).{8,}$/)]],
       phone: ['', Validators.required]
     });
+  }
+
+  regresar(){
+    this.router.navigate(['/tabs/tab4']);
   }
 
   displayImage(event: Event): void {
