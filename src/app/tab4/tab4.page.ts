6f6f2528c6bc020d6bc7825965cfc77b30e32988
BenjaminWebster9876 <2102183@utrivieramaya.edu.mx>
2023-08-09 18:13:46 -0500

    commit

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginPage } from '../login/login.page';
 import { LoginCheckService } from '../services/login-check.service';
+import { Clipboard } from '@capacitor/clipboard';
+import { Toast } from '@capacitor/toast';
 
 
 @Component({
@@ -27,5 +29,16 @@ export class Tab4Page implements OnInit {
   perfil(){
     this.router.navigate(['/perfil']);
   }
+
+  async copyToClipboard(text: string) {
+    await Clipboard.write({ string: text }); 
+    this.showemailUse();
+
+  }
+  showemailUse = async () => {
+    await Toast.show({
+      text: 'Copiado al portapapeles',
+    });
+  };
 }
 
