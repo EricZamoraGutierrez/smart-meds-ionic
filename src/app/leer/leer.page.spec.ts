7a07ab5d83032b756118e52ad9da288f60d89e15
EricZamoraGutierrez <2102430@utrivieramaya.edu.mx>
2024-10-05 14:19:20 -0500

    Little fix

--- src/app/leer/leer.page.spec.ts
+++ src/app/leer/leer.page.spec.ts
@@ -15,3 +15,7 @@ describe('LeerPage', () => {
     expect(component).toBeTruthy();
   });
 });
+
+function async(arg0: () => void): jasmine.ImplementationCallback {
+  throw new Error('Function not implemented.');
+}
