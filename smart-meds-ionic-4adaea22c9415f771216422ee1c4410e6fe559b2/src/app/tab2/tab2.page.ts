020f4140f5a86d7e78c58c076b1bb8332ff13e7c
benja <2102183@utrivieramaya.edu.mx>
2024-11-19 19:19:36 -0500

    mediook

--- smart-meds-ionic-4adaea22c9415f771216422ee1c4410e6fe559b2/src/app/tab2/tab2.page.ts
+++ smart-meds-ionic-4adaea22c9415f771216422ee1c4410e6fe559b2/src/app/tab2/tab2.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Firestore, collection, addDoc, query, where, getDocs, getDoc, deleteDoc, doc} from '@angular/fire/firestore';
+
 
 
 @Component({
@@ -11,7 +13,12 @@ import { NavController } from '@ionic/angular';
 export class Tab2Page {
   selectedSegment: string = 'option1';  // Valor predeterminado
 
-  constructor(private router: Router, private navCtrl: NavController) {this.expanded = true, this.expanded2 = true, this.expanded3 = true, this.expanded4 = true; }
+  constructor(private firestore: Firestore, private router: Router, private navCtrl: NavController) {this.expanded = true, this.expanded2 = true, this.expanded3 = true, this.expanded4 = true; }
+
+  ionViewDidEnter() {
+    this.readPrescriptions();
+    this.data = [];
+  }
 
   expanded: boolean = false;
   expanded2: boolean = false;
@@ -40,11 +47,53 @@ export class Tab2Page {
   
   segmentChanged(event: any) {
     this.selectedSegment = event.detail.value;  // Obtener el valor seleccionado
-    console.log('Segment cambiado a:', this.selectedSegment);
   }
 
   goBack() {
     this.navCtrl.back();
+  }
+
+  
+  data: any = [];
+
+  id: string = '';
+
+  async readPrescriptions() {
+
+    const ref = collection(this.firestore, 'Medicaciones');
+    const q = query(ref)
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+
+      this.data.push({
+        id: doc.data()['Detalles']['MedID'],
+        name: doc.data()['name'],
+        comment: doc.data()['Detalles']['comentario'],
+        interval: '',
+      }
+      );
+    });
+
+    const ref2 = collection(this.firestore, 'Prescripciones');
+    const q2 = query(ref2)
+    const querySnapshot2 = await getDocs(q2);
+    querySnapshot2.forEach((doc) => {
+      var i = 0;
+      for(i=0; i<this.data.length; i++){
+        if(this.data[i]['id'] == doc.data()['MedID']){
+          this.data[i]['interval'] = doc.data()['interval'];
+        }        
+      }
+      
+    });
+
+  }
+  delete(id:any){
+    const ref = doc(this.firestore, 'Medicaciones', id);
+    deleteDoc(ref);
+    this.data = [];
+    this.readPrescriptions();
+
   }
 }
 
